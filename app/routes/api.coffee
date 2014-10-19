08226cf5ac5306ad6c2c4ffb7347437de91666a8
okamuuu <okamuuu@gmail.com>
2014-10-19 17:28:09 +0900

    add routing.

--- app/routes/api.coffee
+++ app/routes/api.coffee
@@ -6,16 +6,19 @@ router.get '/', (req, res)->
 router.get '/phones', (req, res)->
   res.json [
     {
+      "id": "nexus",
       'name': 'Nexus S',
       'snippet': 'Fast just got faster with Nexus S.'
       age: 1
     }
     {
+      "id": "motorola-defy-with-motoblur",
       'name': 'Motorola XOOM™ with Wi-Fi',
       'snippet': 'The Next, Next Generation tablet.'
       age: 2
     }
     {
+      "id": "motorola",
       'name': 'MOTOROLA XOOM™',
       'snippet': 'The Next, Next Generation tablet.'
       age: 3
--- static/js/controller.js
+++ static/js/controller.js
@@ -1,9 +1,15 @@
-var phonecatApp = angular.module('phonecatApp', []);
+var phonecatControllers = angular.module('phonecatControllers', []);
 
-phonecatApp.controller('PhoneListCtrl', function ($scope, $http) {
+phonecatControllers.controller('PhoneListCtrl', function ($scope, $http) {
   $http.get('api/phones').success(function(data) {
     $scope.phones = data;
   });
 
   $scope.orderProp = 'age';
 });
+
+phonecatControllers.controller('PhoneDetailCtrl', ['$scope', '$routeParams',
+  function($scope, $routeParams) {
+    $scope.phoneId = $routeParams.phoneId;
+  }
+]);
